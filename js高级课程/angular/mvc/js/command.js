42871cc1e2ca132d479a804a40c5906dc1a989bc
chenweigit <ck.email2010@gmail.com>
2014-10-14 09:39:13 +0800

    Update command.js
    添加 bind  的控制器

--- js高级课程/angular/mvc/js/command.js
+++ js高级课程/angular/mvc/js/command.js
@@ -1,1 +1,19 @@
-/** * Created with 陈伟. * User: ck * Date: 14-10-12 * Time: 下午9:27 * */function listIfno($scope){     $scope.info = ['屌丝','逆袭'];}
+/**
+ * Created with 陈伟.
+ * User: ck
+ * Date: 14-10-12
+ * Time: 下午9:27
+ *
+ */
+
+/**
+ *   数组的循环
+ * @param $scope
+ */
+function listIfno($scope){
+     $scope.info = ['屌丝','逆袭'];
+}
+
+function bindInfo($scope){
+     $scope.str = 'ng-bind ok';
+}
